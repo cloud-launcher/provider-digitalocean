cb53ba4d10b1f49a59a12bab10fa6324ad5c2bcd
Blake La Pierre <blakelapierre@gmail.com>
2015-03-25 19:05:14 -0700

    digitalocean core (may not be correct)

--- src/provider/core.js
+++ src/provider/core.js
@@ -1,12 +1,14 @@
+import promise from 'promise-callback';
+import _ from 'lodash';
 
-module.exports = provider;
+module.exports = digitalocean;
 
-function provider(API, credentials) {
+function digitalocean(API, credentials) {
   const status = {
     limit: undefined,         //API Rate Limit (per hour?)
     remaining: undefined,     //API Calls Left
     resetTime: undefined,     //When API Rate Calls will reset (datetime)
-    machineLimit: undefined   //Number of machines provider allows you to createwwwwwwwwwww
+    machineLimit: undefined   //Number of machines digitalocean allows you to create
   };
 
   return {
@@ -25,44 +27,139 @@ function provider(API, credentials) {
   };
 
   function createMachine(description) {
-    return new Promise((resolve, reject) => {
-      const machine = {};
-      resolve(machine);
+    const api = getApi(),
+          {id, location, size, image, keys, userData} = machineDescription;
+
+    return promise(
+      api.dropletsCreateNewDroplet.bind(api),
+      id, location, size, image, {ssh_keys: keys, user_data: userData}
+    )
+    .then(handleApiResponse, handleApiError)
+    .then(value => {
+      const [data, header, response] = value,
+            {droplet} = data,
+            doIdentifier = {
+              id: droplet.id,
+              createdAt: droplet.created_at
+            };
+
+      return doIdentifier;
     });
   }
 
   function destroyMachine(machine) {
-    return new Promise((resolve, reject) => {
-      resolve({destroyed: true, machine});
+    var api = getApi();
+
+    return promise(
+      api.dropletsDeleteDroplet.bind(api),
+      machine.response.id
+    )
+    .then(handleApiResponse)
+    .then(value => {
+      return {success: true};
     });
   }
 
   function listMachines() {
-    return new Promise((resolve, reject) => {
-      const machines = [];
-      resolve(machines);
+    const api = getApi();
+
+    return promise(
+      api.dropletsGetAll.bind(api)
+    )
+    .then(handleApiResponse)
+    .then(value => {
+      const [data] = value,
+            {droplets} = data;
+
+      return _.reduce(droplets, (result, droplet) => {
+        const {id, created_at: createdAt, name, networks, status} = droplet;
+
+        result[name] = {
+          id,
+          createdAt,
+          networks: {
+            v4: _.map(networks.v4, network => {
+              const {ip_address: ipAddress, netmask, gateway, type} = network;
+              return {ipAddress, netmask, gateway, type};
+            }),
+            v6: networks.v6
+          },
+          status
+        };
+
+        return result;
+      }, {});
     });
   }
 
   function verifyAccount() {
-    return new Promise((resolve, reject) => {
-      resolve();
+    const api = getApi();
+
+    return promise(
+      api.account.bind(api)
+    )
+    .then(handleApiResponse)
+    .then(value => {
+      const [data] = value;
+
+      status.machineLimit = data.account.droplet_limit;
     });
   }
 
   function getAPI() {
-    return new API(credentials);
+    return new API(credentials.token, 1000);
+  }
+
+  function handleApiResponse(value) {
+    return new Promise((resolve, reject) => {
+      let data, response;
+
+      if (Array.isArray(value)) [data, response] = value;
+      else data = response = value;
+
+      response = response || data;
+
+      const headers = response.headers || getResponseHeaders(response, ['RateLimit-Limit', 'RateLimit-Remaining', 'RateLimit-Reset']);
+
+      status.limit = parseInt(headers['RateLimit-Limit'] || '0');
+      status.remaining = parseInt(headers['RateLimit-Remaining'] || '0');
+      status.reset = new Date(parseInt(headers['RateLimit-Reset'] || '0') * 1000);
+
+      resolve(value);
+    });
+  }
+
+  function handleApiError(value) {
+    return new Promise((resolve, reject) => {
+      const {error, args} = value;
+      let [data, response] = args;
+
+      response = response || data;
+
+      if (response.statusCode === 401) {
+        reject({error: 'Unauthorized', data, response, provider: digitalocean.$name});
+        return;
+      }
+      reject({error, data, response, provider: digitalocean.$name});
+    });
+  }
+
+  function getResponseHeaders(response, headers) {
+    return _.reduce(headers, (result, header) => {
+      result[header] = response.getResponseHeader(header);
+      return result;
+    }, {});
   }
 }
 
-provider.$name = 'provider';
-provider.$targets = ['coreos'];
-provider.$dashboardUrl = 'https://dashboard_link';
-provider.$referralUrl= 'https://referral_link';
-provider.$credentialSchema = {
+digitalocean.$name = 'digitalocean';
+digitalocean.$targets = ['coreos'];
+digitalocean.$dashboardUrl = 'https://cloud.digitalocean.com';
+digitalocean.$referralUrl= 'https://www.digitalocean.com/?refcode=4df1a6f6f727';
+digitalocean.$credentialSchema = {
   token: {
     type: 'string',
-    header: 'Some kind of autthorization token',
-    link: 'https://link_to_create_new_token'
+    header: 'Personal Access Token',
+    link: 'https://cloud.digitalocean.com/settings/tokens/new'
   }
 };
