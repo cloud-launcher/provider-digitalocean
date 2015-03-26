532a5cbc645df5ba28aaaa2a16b7495280c6a8ed
Blake La Pierre <blakelapierre@gmail.com>
2015-03-25 19:04:02 -0700

    This isn't correct (need to return a body and response type object), but just a starting point.

--- src/provider/simulator.js
+++ src/provider/simulator.js
@@ -1,5 +1,72 @@
-module.exports = credentials => core(simulator, credentials);
 
-function simulator() {
+// import digitalocean from './simulator';
+// API = digitalocean(profile)(credentials);
+module.exports = profile => credentials => core(simulator(profile), credentials);
 
+function simulator(profile) {
+  const state = {
+    machines: []
+  };
+
+  const images = {
+    'coreos-alpha': true,
+    'coreos-beta': true,
+    'coreos-stable': true
+  };
+
+  const {sizes, locations} = profile;
+
+  // This implements part of the `do-wrapper` interface
+  return credentials => {
+    return {
+      dropletsCreateNewDroplet,
+      dropletsDeleteDroplet,
+      dropletsGetAll,
+      account
+    };
+
+    function dropletsCreateNewDroplet(name, location, size, image, options, callback) {
+      const {ssh_keys, user_data} = options || {};
+
+      if (machines[name]) {
+        callback(new Error(`Machine ${name} already exists!`));
+        return;
+      }
+
+      if (!sizes[size]) {
+        callback(new Erorr(`Size ${size} doesn't exist!`));
+        return;
+      }
+
+      if (!locations[location]) {
+        callback(new Error(`Location ${location} doesn't exist!`));
+        return;
+      }
+
+      const machine = {
+        name,
+        location,
+        size,
+        image,
+        options
+      };
+
+      machines[name] = machine;
+
+      return machine;
+    }
+
+    function dropletsDeleteDroplet(name, callback) {
+
+    }
+
+    function dropletsGetAll(callback) {
+
+    }
+
+    function account(callback) {
+      if (credentials) callback(undefined, true);
+      else callback(new Error('Credentials not defined!'));
+    }
+  };
 }
