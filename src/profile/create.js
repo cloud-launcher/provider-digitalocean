ffc3488e4feb89631e8058650b3ddd92dbe91322
Blake La Pierre <blakelapierre@gmail.com>
2015-03-26 15:33:02 -0700

    Add physical location coordinates.

--- src/profile/create.js
+++ src/profile/create.js
@@ -2,6 +2,21 @@ import _ from 'lodash';
 
 const blacklist = {locations: ['nyc2']};
 
+// Note: These are just generic coordinates for the city, not the actual
+//  location of the datacenters. If you know the actual locations, please
+//  update!
+const physicalLocations = {
+  ams1: {lat: 52.370216, long: 4.895168},
+  ams2: {lat: 52.370216, long: 4.895168},
+  ams3: {lat: 52.370216, long: 4.895168},
+  lon1: {lat: 51.507351, long: -0.127758},
+  nyc1: {lat: 40.712784, long: -74.005941},
+  nyc2: {lat: 40.712784, long: -74.005941},
+  nyc3: {lat: 40.712784, long: -74.005941},
+  sgp1: {lat: 1.352083,  long: 103.819836},
+  sfo1: {lat: 37.774929, long: -122.419416}
+};
+
 module.exports = (DOWrapper, DOToken) => {
   const api = new DOWrapper(DOToken);
 
@@ -48,6 +63,7 @@ module.exports = (DOWrapper, DOToken) => {
         regions[id] = {
           id,
           vicinity: name,
+          physicalLocation: physicalLocations[id] || {},
           sizes
         };
       }, {}));
